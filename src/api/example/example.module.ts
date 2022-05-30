9315decc09c129b7aa40684c1de6de95015b2e71
Henrique Leite <henriqueleite616@gmail.com>
2022-05-30 10:40:12 -0300

    Change Package Name (#1)

--- src/api/example/example.module.ts
+++ src/api/example/example.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
-import { SymbiosisModule } from "@techmmunity/symbiosis-nestjs";
+import { ThothOMModule } from "@thothom/nestjs";
 
 import { ExampleController } from "./example.controller";
 import { ExampleEntity } from "./example.entity";
 import { ExampleService } from "./example.service";
 
 @Module({
-	imports: [SymbiosisModule.forFeature([ExampleEntity])],
+	imports: [ThothOMModule.forFeature([ExampleEntity])],
 	controllers: [ExampleController],
 	providers: [ExampleService],
 })
